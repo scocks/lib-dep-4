075d14e2ef786e7fd04248f3b0a6d94e76ac8479
Steven Cocks <ext.steven.cocks@king.com>
2024-03-19 15:00:34 +0100

    Refactor methods

--- src/main/java/org/king/libDep4/Library.java
+++ src/main/java/org/king/libDep4/Library.java
@@ -1,13 +1,12 @@
 package org.king.libdep4;
-public class Library {
-    public String[] count(Integer number) {
-        String[] lines = new String[number];
-        for (int i = 0; i < number; i++) {
-            lines[i] = "i = " + i;
-        }
-        return lines;
-    }
+public class Library {    
     public String greet(String name) {
-        return "Hej, " + name + "!";
+        return "greet from libdep4, " + name + "!";
+    }
+    public String greet1(String name) {
+        return "greet1 from libdep4, " + name + "!";
+    }
+    public String greet2(String name) {
+        return "greet2 from libdep4, " + name + "!";
     }
 }
--- src/test/java/org/king/libDep4/MainTest.java
+++ src/test/java/org/king/libDep4/MainTest.java
@@ -1,24 +1,26 @@
 package org.king.libdep4;
 
 import org.junit.jupiter.api.Test;
-import org.king.libdep4.Library;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MainTest {
-    @Test
-    void testCount() {
-        Library library = new Library();
-        String[] result = library.count(10);
-        assertEquals(10, result.length);
-        for (int i = 0; i < 10; i++) {
-            assertEquals("i = " + i, result[i]);
-        }
-    }
+class MainTest {    
     @Test
     void testGreet() {
         Library library = new Library();
         String result = library.greet("lib-dep-4");
-        assertEquals("Hej, lib-dep-4!", result);
+        assertEquals("greet from libdep4, lib-dep-4!", result);
+    }
+    @Test
+    void testGreet1() {
+        Library library = new Library();
+        String result = library.greet1("lib-dep-4");
+        assertEquals("greet1 from libdep4, lib-dep-4!", result);
+    }
+    @Test
+    void testGreet2() {
+        Library library = new Library();
+        String result = library.greet2("lib-dep-4");
+        assertEquals("greet2 from libdep4, lib-dep-4!", result);
     }
 }
