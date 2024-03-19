534dce1b074b86e7a4fa7c9456cbb130394f662a
Steven Cocks <ext.steven.cocks@king.com>
2024-03-19 16:37:01 +0100

    drop!

--- src/main/java/org/king/libDep4/Library.java
+++ src/main/java/org/king/libDep4/Library.java
@@ -1,12 +1,12 @@
 package org.king.libdep4;
 public class Library {    
     public String greet(String name) {
-        return "greet from libdep4, " + name + "!";
+        return "greet from libdep4, " + name;
     }
     public String greet1(String name) {
-        return "greet1 from libdep4, " + name + "!";
+        return "greet1 from libdep4, " + name;
     }
     public String greet2(String name) {
-        return "greet2 from libdep4, " + name + "!";
+        return "greet2 from libdep4, " + name;
     }
 }
--- src/test/java/org/king/libDep4/MainTest.java
+++ src/test/java/org/king/libDep4/MainTest.java
@@ -9,18 +9,18 @@ class MainTest {
     void testGreet() {
         Library library = new Library();
         String result = library.greet("lib-dep-4");
-        assertEquals("greet from libdep4, lib-dep-4!", result);
+        assertEquals("greet from libdep4, lib-dep-4", result);
     }
     @Test
     void testGreet1() {
         Library library = new Library();
         String result = library.greet1("lib-dep-4");
-        assertEquals("greet1 from libdep4, lib-dep-4!", result);
+        assertEquals("greet1 from libdep4, lib-dep-4", result);
     }
     @Test
     void testGreet2() {
         Library library = new Library();
         String result = library.greet2("lib-dep-4");
-        assertEquals("greet2 from libdep4, lib-dep-4!", result);
+        assertEquals("greet2 from libdep4, lib-dep-4", result);
     }
 }
